0ea5f11ab9d36a4ff14ba079cf8146e2ddec8493
IurieChi <chigaiiura@yahoo.com>
2024-02-09 14:53:32 +0100

    added new functinality date format to be added image name

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-import os
+
 import PySimpleGUI as sg
 import rename_img_createdday as re
 
@@ -8,6 +8,8 @@ sg.theme('DarkTeal2')
 form_rows = [ 
     [sg.Text('Add created date to image from repository.')],
     [sg.Text('Image path:', size=(10,1)),sg.Input(key='path'),sg.FolderBrowse()], 
+    [sg.Text('Select required format to be added to image: date, month, year:')],
+    [sg.Checkbox('Day',key='d'),sg.Checkbox('Month',key='m'),sg.Checkbox('Year',key='y')],
     [sg.Text('Select image type:')],
     [sg.Checkbox('Select All:', key= 'all')],
     [sg.Checkbox('JPG',key='.jpg'),sg.Checkbox('PNG',key='.png'),sg.Checkbox('GIF',key='.gif'),sg.Checkbox('RAV',key='.rav'),sg.Checkbox('SVG',key='.svg'),sg.Checkbox('Bitmap', key='.bmp'),sg.Checkbox('HEIC',key='.HEIC')],
@@ -21,26 +23,33 @@ while True:
     
     folder_path = value['path'] #save path to variable
     file_type = [] #create ampty aray for file types
+    date_format = '' #format date 
 
     if event == 'Rename Img':
         if folder_path == '':
-            window['-OUTPUT-'].update("Select folder",text_color='yellow')
-        elif value['all']:
-            file_type = ['.jpg','.png','.gif','.bmp','.rav','.svg','.HEIC']
-            re.rename_image(folder_path,file_type)
-            window['-OUTPUT-'].update("Succes",text_color='green')
+            window['-OUTPUT-'].update('Select folder! ',text_color='yellow')
+        elif value ['d'] != True and value['m']!= True and value['y']!=True:
+            window['-OUTPUT-'].update('Please choose date format!',text_color = 'orange')
         else:
-            if value['.jpg'] !=True and value['.png'] !=True and value['.gif']!=True and value['.bmp']!=True and value['.rav'] !=True and value['.HEIC'] !=True and value['.svg'] !=True:
-                window['-OUTPUT-'].update('Select image type to be renamed',text_color = 'white')
+            for checkbox_key_date in ['d','m','y']:
+                if value[checkbox_key_date]:
+                    date_format += '%'+ checkbox_key_date + '_'
+            if value['all']:
+                file_type = ['.jpg','.png','.gif','.bmp','.rav','.svg','.HEIC']
+                re.rename_image(folder_path,file_type,date_format)
+                window['-OUTPUT-'].update("Succes",text_color='green')
             else:
-                window['-OUTPUT-'].update("")
-                for checkbox_key in ['.jpg','.png','.gif','.bmp','.rav','.svg','.HEIC']:
-                        if value [checkbox_key]:
-                            file_type.append(checkbox_key)
+                if value['.jpg'] !=True and value['.png'] !=True and value['.gif']!=True and value['.bmp']!=True and value['.rav'] !=True and value['.HEIC'] !=True and value['.svg'] !=True:
+                    window['-OUTPUT-'].update('Select image type to be renamed',text_color = 'white')
+                else:
+                    window['-OUTPUT-'].update("")
+                    for checkbox_key in ['.jpg','.png','.gif','.bmp','.rav','.svg','.HEIC']:
+                            if value [checkbox_key]:
+                                file_type.append(checkbox_key)
 
-                re.rename_image(folder_path,file_type)
-                window['-OUTPUT-'].update("Succes",text_color='white')
-                # window['-OUTPUT-'].update(file_type,text_color='yellow')
+                    re.rename_image(folder_path,file_type,date_format)
+                    window['-OUTPUT-'].update("Succes",text_color='white')
+                    # window['-OUTPUT-'].update(file_type,text_color='yellow')
  
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
--- rename_img_createdday.py
+++ rename_img_createdday.py
@@ -1,72 +1,30 @@
-#Look on directory check metata data and rename photo with created day
+#Look on directory check metata data and rename photo with created day, month,  year 
 
 from datetime import datetime
-from pathlib import Path
 import os
-import PySimpleGUI as sg
+from pathlib import Path
 
-def generate_created_date(path):
+
+def generate_created_date(path,data_format):
     stat_result = path.stat()
     creation_day = stat_result.st_birthtime
     # Windows --> stat_result.st_ctime
 	# Other Unix --> stat_result.st_ctime (last modification date)
 	# Other Linux --> stat_result.st_mtime (last modification date)
     timestamp = datetime.utcfromtimestamp(creation_day)
-    return timestamp.strftime('%m_%y_')
+    return timestamp.strftime(data_format)
 
-def rename_image(image_folder, type_file):
+def rename_image(image_folder, type_file,data_format):
     os.chdir(image_folder)
     # type_file = ['png']
     for path in Path(image_folder).iterdir():
         if path.is_file()and path.suffix in type_file:
             # print(f"Rename {path.stem}" )
-            date = generate_created_date(path)
+            date = generate_created_date(path,data_format)
             new_name = Path(date + path.stem + path.suffix)
             path.rename(new_name)
             
 
 # ('images')    
 
-# GUI
-sg.theme('DarkTeal2')
 
-form_rows = [ 
-    [sg.Text('Add created date to image from repository.')],
-    [sg.Text('Image path:', size=(10,1)),sg.Input(key='path'),sg.FolderBrowse()], 
-    [sg.Text('Select image type:')],
-    [sg.Checkbox('Select All:', key= 'all')],
-    [sg.Checkbox('JPG',key='.jpg'),sg.Checkbox('PNG',key='.png'),sg.Checkbox('GIF',key='.gif'),sg.Checkbox('RAV',key='.rav'),sg.Checkbox('SVG',key='.svg'),sg.Checkbox('Bitmap', key='.bmp'),sg.Checkbox('HEIC',key='.HEIC')],
-    [sg.Text(size=(40,1), key='-OUTPUT-')],
-    [sg.Button('Rename Img'), sg.Button('Exit')], 
-]
-
-window = sg.Window('Rename Image from repository', form_rows)
-while True:
-    event , value = window.read()
-    
-    folder_path = value['path'] #save path to variable
-    file_type = [] #create ampty aray for file types
-
-    if event == 'Rename Img':
-        if folder_path == '':
-            window['-OUTPUT-'].update("Select folder",text_color='yellow')
-        elif value['all']:
-            file_type = ['.jpg','.png','.gif','.bmp','.rav','.svg','.HEIC']
-            rename_image(folder_path,file_type)
-            window['-OUTPUT-'].update("Succes",text_color='green')
-        else:
-            if value['.jpg'] !=True and value['.png'] !=True and value['.gif']!=True and value['.bmp']!=True and value['.rav'] !=True and value['.HEIC'] !=True and value['.svg'] !=True:
-                window['-OUTPUT-'].update('Select image type to be renamed',text_color = 'white')
-            else:
-                window['-OUTPUT-'].update("")
-                for checkbox_key in ['.jpg','.png','.gif','.bmp','.rav','.svg','.HEIC']:
-                        if value [checkbox_key]:
-                            file_type.append(checkbox_key)
-
-                rename_image(folder_path,file_type)
-                window['-OUTPUT-'].update("Succes",text_color='white')
-                # window['-OUTPUT-'].update(file_type,text_color='yellow')
- 
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-window.close()
