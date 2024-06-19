250e6c9d44518ef42c3bf09d672e499c5ea07809
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-06-19 17:01:37 +0200

    fixed bug if image has similar meta data then app save only last image, now added counter for image to avoid same name for images

--- rename_img_createdday.py
+++ rename_img_createdday.py
@@ -12,18 +12,20 @@ def generate_created_date(path,data_format):
 	# Other Unix --> stat_result.st_ctime (last modification date)
 	# Other Linux --> stat_result.st_mtime (last modification date)
     timestamp = datetime.utcfromtimestamp(creation_day)
-    print(timestamp)
+    # print(timestamp.strftime(data_format))
     return timestamp.strftime(data_format)
 
 def rename_image(image_folder, type_file,data_format):
     os.chdir(image_folder)
-    
+    counter=0
     for path in Path(image_folder).iterdir():
+        
         if path.is_file()and path.suffix in type_file:
-            # print(f"Rename {path.stem}" )
+            print(f"Rename {path.stem}" )
             date = generate_created_date(path,data_format)
-            new_name = Path(date + path.stem + path.suffix)
+            new_name = Path(date+str(counter)+path.suffix)#+ path.stem 
             path.rename(new_name)
+        counter+=1
             
 
 # ('images')    
